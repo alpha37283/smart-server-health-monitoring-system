4063ddaf163f2cf3981cb5966f19e82997003283
alpha37283 <codewithmuneeb7@gmail.com>
2026-03-16 23:30:13 +0500

    traffic calculation B

--- agent/collector/network/traffic.py
+++ agent/collector/network/traffic.py
@@ -3,6 +3,61 @@
 import psutil
 import time
 
+prev_bytes_received = None
+prev_bytes_sent = None
+prev_timestamp = None
+
+def bytes_sent_per_second():
+    global prev_bytes_sent, prev_timestamp
+
+    current_bytes_sent = psutil.net_io_counters().bytes_sent
+    current_time = time.time()
+
+    # first run
+    if prev_bytes_sent is None or prev_timestamp is None:
+        prev_bytes_sent = current_bytes_sent
+        prev_timestamp = current_time
+        return None
+
+    diff_bytes = current_bytes_sent - prev_bytes_sent
+    diff_time = current_time - prev_timestamp
+
+    # update previous values
+    prev_bytes_sent = current_bytes_sent
+    prev_timestamp = current_time
+
+    if diff_time <= 0 or diff_bytes < 0:
+        return None
+
+    return diff_bytes / diff_time
+
+
+
+def bytes_received_per_second():
+    global prev_bytes_received, prev_timestamp
+
+    current_bytes_received = psutil.net_io_counters().bytes_recv
+    current_time = time.time()
+
+    # first run
+    if prev_bytes_received is None or prev_timestamp is None:
+        prev_bytes_received = current_bytes_received
+        prev_timestamp = current_time
+        return None
+
+    diff_bytes = current_bytes_received - prev_bytes_received
+    diff_time = current_time - prev_timestamp
+
+    # update previous values
+    prev_bytes_received = current_bytes_received
+    prev_timestamp = current_time
+
+    if diff_time <= 0 or diff_bytes < 0:
+        return None
+
+    return diff_bytes / diff_time   
+    
+
 
 async def collect_network_traffic(event_bus):
     """
